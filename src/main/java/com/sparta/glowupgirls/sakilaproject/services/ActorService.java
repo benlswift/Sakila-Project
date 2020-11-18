44d336be4909a664060c9e457ec09c430243d944
ChangoTheBeast <33813476+BoboTheNameless@users.noreply.github.com>
2020-11-18 10:32:34 +0000

    Upload of actors.html, ActorController and update of ActorService

--- src/main/java/com/sparta/glowupgirls/sakilaproject/services/ActorService.java
+++ src/main/java/com/sparta/glowupgirls/sakilaproject/services/ActorService.java
@@ -1,10 +1,21 @@
 package com.sparta.glowupgirls.sakilaproject.services;
 
+import com.sparta.glowupgirls.sakilaproject.entities.Actor;
 import com.sparta.glowupgirls.sakilaproject.repositories.ActorRepositories;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ActorService {
 
     private ActorRepositories actorRepositories;
+
+    public ActorService(ActorRepositories actorRepositories) {
+        this.actorRepositories = actorRepositories;
+    }
+
+    public List<Actor> getAllActors() {
+        return (List<Actor>) actorRepositories.findAll();
+    }
 }
