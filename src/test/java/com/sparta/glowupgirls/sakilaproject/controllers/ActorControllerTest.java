2510e8b575187187fb46577df43bf99bf9964573
ChangoTheBeast <33813476+BoboTheNameless@users.noreply.github.com>
2020-11-18 12:24:34 +0000

    Addition of test for ActorController

--- src/test/java/com/sparta/glowupgirls/sakilaproject/controllers/ActorControllerTest.java
+++ src/test/java/com/sparta/glowupgirls/sakilaproject/controllers/ActorControllerTest.java
@@ -1,21 +1,15 @@
 package com.sparta.glowupgirls.sakilaproject.controllers;
 
-import com.sparta.glowupgirls.sakilaproject.repositories.ActorRepositories;
-import com.sparta.glowupgirls.sakilaproject.services.ActorService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
