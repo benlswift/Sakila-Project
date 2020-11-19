aafd8e0a023d5e2bc7ea112d7637ee62845c6b60
ChangoTheBeast <33813476+BoboTheNameless@users.noreply.github.com>
2020-11-19 12:07:58 +0000

    Updated CSS for Films and FilmInfo Pages. Added links from actor to film info

--- src/main/java/com/sparta/glowupgirls/sakilaproject/controllers/ActorController.java
+++ src/main/java/com/sparta/glowupgirls/sakilaproject/controllers/ActorController.java
@@ -12,8 +12,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,7 +51,7 @@ public class ActorController {
         List<FilmActor> filmActors = filmActorService.getFilmActorsByActorId(id);
         List<Film> films = new ArrayList<>();
         for (FilmActor filmActor : filmActors) {
-            Film film = filmService.getFilmByID(filmActor.getFilmId());;
+            Film film = filmService.getFilmByID(filmActor.getFilmId());
             films.add(film);
         }
         modelMap.addAttribute("actor", actor);
